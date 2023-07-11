3ee481d262c2e196b95fa4561565eec8e4c3df29
Okami <maks.chmiel98@gmail.com>
2023-07-11 13:33:05 +0200

    Expanded visualisations

--- main.R
+++ main.R
@@ -29,13 +29,23 @@ head(res)
 print('Genes with highest absolute log2FC value')
 head(res[order(abs(res$log2FoldChange), decreasing = TRUE), ])
 
-# looking for genes with both very high absolute fold change as well as high padj
-small_padj = head(row.names(res), 20)
-high_fc = head(row.names(res[order(abs(res$log2FoldChange), decreasing = TRUE), ]), 20)
-print('Genes with both small padj and high fold change')
-intersect(small_padj, high_fc)
-
-par(mfrow=c(1, 3))
+# ploting counts of 6 genes with smallest padj
+small_padj = head(row.names(res), 6)
+par(mfrow = c(1, 3))
 for (gene in small_padj) {
   plotCounts(dds, gene = gene, intgroup = 'sample_group')
 }
+
+# looking for genes with both very high absolute fold change as well as small padj
+small_padj = row.names(res[res$padj < 0.01, ])
+high_fc = row.names(res[abs(res$log2FoldChange) > 1.5, ])
+small_padj_x_high_fc = intersect(small_padj, high_fc)
+
+# plotting vulcanoplot showing relation between FC and padj and pointing out genes found using sets
+par(mfrow = c(1,1))
+with(res, plot(log2FoldChange, -log10(pvalue), pch = 20, main = "Volcano plot"))
+with(subset(res, padj < 0.01 ), points(log2FoldChange, -log10(pvalue), pch = 20, col = "blue"))
+with(subset(res, padj < 0.01 & abs(log2FoldChange) > 1.5), points(log2FoldChange, -log10(pvalue), pch = 20, col = "red"))
+
+# ploitting MA visualisation
+plotMA(res)
