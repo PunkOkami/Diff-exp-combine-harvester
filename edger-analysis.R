1454dac2d22add473befa1eec025343bbc0fca73
Okami <maks.chmiel98@gmail.com>
2023-07-13 11:48:49 +0200

    Expanded edgeR analysis a bit further and added visualisations

--- edger-analysis.R
+++ edger-analysis.R
@@ -1,5 +1,7 @@
+library(limma)
 library(edgeR)
 library(tximport)
+library(pheatmap)
 
 # setting sample names and creating factor assigning samples to groups
 control_samples = c('SRR1747395', 'SRR1747397', 'SRR1747399')
@@ -13,22 +15,29 @@ names(files) = samples
 data <- tximport(files, type = "salmon", tx2gene = NULL, txIn = FALSE, geneIdCol = "Name")
 
 # transforms data into format used by edgeR and performs basic
-data = DGEList(data$counts, group = sample_factor)
-data = calcNormFactors(data)
-data = estimateCommonDisp(data)
-data = estimateTagwiseDisp(data)
+edgar_data = DGEList(data$counts, group = sample_factor)
+edgar_data = calcNormFactors(edgar_data)
+edgar_data = estimateCommonDisp(edgar_data)
+edgar_data = estimateTagwiseDisp(edgar_data)
 
 # plots MDS plot to show how samples compare
-plotMDS(data, method="bcv", col=as.numeric(data$samples$group))
-legend("bottomleft", as.character(unique(data$samples$group)), col=1:2, pch=20)
+plotMDS(edgar_data, col=as.numeric(edgar_data$samples$group), main = 'Sample similarity')
+legend("bottomleft", as.character(unique(edgar_data$samples$group)), col=1:2, pch=20)
 
 # perofming test to see differential expression of genes
-edgar_results = exactTest(data)
+edgar_results = exactTest(edgar_data)
 
 # showing summary of differential expression
 print('Summary of gene expression')
-de_genes = decideTestsDGE(edgar_results, p.value = 0.05, lfc = 1.5)
+de_genes = decideTestsDGE(edgar_results, lfc = 1.5)
 summary(de_genes)
+important_genes = rownames(de_genes)[which(de_genes != 0)]
+print('Names of genes with significant differential expression: ')
+print(important_genes)
+plotSmear(edgar_results, de.tags = important_genes, main = 'Mean difference plot')
+
+important_genes_counts = data$counts[which(rownames(data$counts) %in% important_genes), ]
+pheatmap(important_genes_counts, cluster_cols = FALSE, color = hcl.colors(50, "cyan-magenta"))
 
 # geting results of de
 edgar_results = topTags(edgar_results, n=Inf)
@@ -37,5 +46,5 @@ edgar_results = edgar_results[edgar_results$FDR < 0.05 & complete.cases(edgar_re
 edgar_results = edgar_results[order(edgar_results$FDR, decreasing = FALSE), ]
 
 # saving data to a file
-edgar_results =  cbind(data.frame(Geneid=rownames(edgar_results), Fold_change=2**edgar_results$logFC), edgar_results)
+edgar_results =  cbind(data.frame(Geneid=rownames(edgar_results), Fold_change=2^edgar_results$logFC), edgar_results)
 write.table(edgar_results, file = "Results/edger_results.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
